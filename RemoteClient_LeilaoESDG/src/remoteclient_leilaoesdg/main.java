77f74d0069fef64878961608d7973ce50f446920
diogogomes77 <diogo.gomes77@gmail.com>
2016-12-17 15:28:59 +0000

    Estrutura base

---
+++ RemoteClient_LeilaoESDG/src/remoteclient_leilaoesdg/main.java
@@ -0,0 +1,10 @@
+
+package remoteclient_leilaoesdg;
+
+public class main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ ServerLeilaoESDG/ServerLeilaoESDG-ejb/src/java/leilao/UtilizadorStatefull.java
@@ -0,0 +1,11 @@
+
+package leilao;
+
+import javax.ejb.Stateful;
+
+@Stateful
+public class UtilizadorStatefull implements UtilizadorStatefullRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
