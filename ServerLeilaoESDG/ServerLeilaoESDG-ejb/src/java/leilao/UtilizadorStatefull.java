bff7600f3a7282a0538b0e2e01d0080e1f740c39
diogogomes77 <diogo.gomes77@gmail.com>
2016-12-17 16:29:19 +0000

    add teste remote

--- ServerLeilaoESDG/ServerLeilaoESDG-ejb/src/java/leilao/UtilizadorStatefull.java
+++ ServerLeilaoESDG/ServerLeilaoESDG-ejb/src/java/leilao/UtilizadorStatefull.java
@@ -6,6 +6,12 @@ import javax.ejb.Stateful;
 @Stateful
 public class UtilizadorStatefull implements UtilizadorStatefullRemote {
 
+    @Override
+    public int teste() {
+        return 123;
+    }
+
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+    
 }
