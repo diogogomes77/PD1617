9e9500ba4dcc5c0d3430544ff5bf7cfe65612c28
diogogomes77 <diogo.gomes77@gmail.com>
2016-12-19 15:40:38 +0000

    add AffableBean example

---
+++ LeilaoESDG/ServerLeilaoESDG/ServerLeilaoESDG-ejb/src/java/remotebeans/UtilizadorStatefull.java
@@ -0,0 +1,17 @@
+
+package remotebeans;
+
+import javax.ejb.Stateful;
+
+@Stateful
+public class UtilizadorStatefull implements UtilizadorStatefullRemote {
+
+    @Override
+    public int teste() {
+        return 123;
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+}
